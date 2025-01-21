c08de30ba979cf21c44af025363fe96bd87df94f
Pwu <pwu.dev@outlook.com>
2025-01-21 23:41:29 +0100

    deploy

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,7 @@
 # Use an official Python runtime as a parent image
 FROM python:3.12-slim
 
-# Set the working directory in the container
-WORKDIR /app
+ENV PYTHONUNBUFFERED True
 
 # Copy the requirements file and install dependencies
 COPY requirements.txt .
@@ -11,11 +10,9 @@ RUN pip install -r requirements.txt
 # Copy the entire application code into the container
 COPY . .
 
-# Set an environment variable that our main.py will use to get react app folder
-ENV REACT_APP_BUILD_PATH client/dist
-
 # Expose port 8080 for the Flask app
 EXPOSE 8080
 
 # Command to run the Flask application
-CMD ["uvicorn", "main:app", "--host", "localhost", "--port", "8080"]
+# CMD uvicorn main:app --host 0.0.0.0 --port 8080
+CMD uvicorn main:app --host localhost --port 8080
--- main.py
+++ main.py
@@ -1,46 +1,4 @@
-# from flask import Flask, request, jsonify, send_from_directory
-# from flask_cors import CORS
-# import os
-
-# REACT_APP_BUILD_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'client', 'dist'))
-
-# print("REACT_APP_BUILD_PATH:", REACT_APP_BUILD_PATH)
-
-# app = Flask(__name__, static_folder='client/dist')
-# CORS(app)  # Enable CORS for all routes
-
-# @app.route('/api/submit', methods=['POST'])
-# def submit_data():
-#     data = request.get_json()
-#     print('Received data:', data)
-#     return jsonify({'message': 'Data received successfully!'}), 200
-
-
-# # Serve React App
-# @app.route('/', defaults={'path': ''}, methods=['GET'])
-# @app.route('/<path:path>')
-# def serve(path):
-#     if path != "" and os.path.exists(app.static_folder + '/' + path):
-#         return send_from_directory(app.static_folder, path)
-#     else:
-#         return send_from_directory(app.static_folder, 'index.html')
-    
-
-
-# if __name__ == '__main__':
-#     # This is only for local testing. Deployment to Cloud Functions uses the `main` function.
-#     app.run(debug=True, host='0.0.0.0', port=8080)
-
-#     """ Production """
-#     # from waitress import serve
-#     # serve(app, host="0.0.0.0", port=8080)
-
-
-"""
-gcloud functions deploy scanner --gen2 --runtime python312 --region=europe-west9 --trigger-http --allow-unauthenticated --source . --entry-point=main
-"""
-
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
@@ -48,44 +6,54 @@ from pathlib import Path
 from pydantic import BaseModel
 
 # Get react app build folder
-REACT_APP_BUILD_PATH = "./client/dist"
+REACT_APP_BUILD_PATH = "client/dist"
 
 
 app = FastAPI()
 
-# Enable CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Allow all origins. For production, configure this properly
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# # Enable CORS
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],  # Allow all origins. For production, configure this properly
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
-# Define data model for submit route request
-class SubmitData(BaseModel):
-    key1: str
-    key2: int
-    # Other required/optional fields
+# # Define data model for submit route request
+# class SubmitData(BaseModel):
+#     key1: str
+#     key2: int
+#     # Other required/optional fields
 
-@app.post("/api/submit")
-async def submit_data(data: SubmitData):
-    print("Received data:", data)
-    return {"message": "Data received successfully!"}
+# @app.post("/api/submit")
+# async def submit_data(data: SubmitData):
+#     print("Received data:", data)
+#     return {"message": "Data received successfully!"}
 
-# Serve React App (Adjusted to use pathlib)
-app.mount("/", StaticFiles(directory=REACT_APP_BUILD_PATH, html=True), name="static")
+# # Serve React App (Adjusted to use pathlib)
+# app.mount("/", StaticFiles(directory=REACT_APP_BUILD_PATH, html=True), name="static")
 
 
-@app.get("/{path:path}")
-async def serve_frontend(path: str, request: Request):
-    
-    static_file_path = Path(REACT_APP_BUILD_PATH) / path
-    if static_file_path.is_file():
-       return FileResponse(static_file_path)
+# @app.get("/{path:path}")
+# async def serve_frontend(path: str):
+#     print("path", path)
+#     return "test"
+#     static_file_path = Path(REACT_APP_BUILD_PATH) / path
+#     if static_file_path.is_file():
+#        return FileResponse(static_file_path)
 
-    index_file_path = Path(REACT_APP_BUILD_PATH) / "index.html"
-    return FileResponse(index_file_path)
+#     index_file_path = Path(REACT_APP_BUILD_PATH) / "index.html"
+#     print("i", index_file_path)
+#     return FileResponse(index_file_path)
+
+@app.get("/")
+async def serve_frontend():
+    return "test"
+
+    # index_file_path = Path(REACT_APP_BUILD_PATH) / "index.html"
+    # print("i", index_file_path)
+    # return FileResponse(index_file_path)
 
 
 # if __name__ == "__main__":
@@ -95,3 +63,6 @@ async def serve_frontend(path: str, request: Request):
 
 
 # gcloud run deploy scanner --region us-central1 --allow-unauthenticated
+"""
+gcloud run deploy scanflow1 --region=europe-west9 --min-instances=1 --concurrency=20 --memory=258Mi --allow-unauthenticated --timeout=60 --source .
+"""

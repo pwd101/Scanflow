cda0b0556e5fca82523844cd9c40f9b9a46e6fc7
Pwu <pwu.dev@outlook.com>
2025-01-27 00:34:14 +0100

    v1

--- main.py
+++ main.py
@@ -4,28 +4,29 @@ import os
 from pubsub import publish_message
 from google.cloud import firestore
 
+
 PROJECT_ID = "qr-code-decideomtour-scanflow" 
-TOPIC_ID = "qr-code-scan"
+REACT_APP_BUILD_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'client', 'dist'))
 
-# message_dict = {"name": "John Doe", "age": 30}
-# publish_message(PROJECT_ID, TOPIC_ID, message_dict)
+SCAN_TOPIC_ID = "scanflow-scan-event-topic"
 
-firestore_db = firestore.Client(project=PROJECT_ID)
-docref = doc_ref = firestore_db.collection("test_coll").document("test_doc")
-doc_ref_infos = doc_ref.get().to_dict()
-print(doc_ref.get().to_dict())
+pubsub_msg_dict  = {"name" : "Alex", "surname" : "Io"}
+publish_message(PROJECT_ID, SCAN_TOPIC_ID, pubsub_msg_dict)
 
-# REACT_APP_BUILD_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), 'client', 'dist'))
 
-# print("REACT_APP_BUILD_PATH:", REACT_APP_BUILD_PATH)
+# firestore_db = firestore.Client(project=PROJECT_ID)
+# docref = doc_ref = firestore_db.collection("test_coll").document("test_doc")
+# doc_ref_infos = doc_ref.get().to_dict()
+# print(doc_ref.get().to_dict())
 
 # app = Flask(__name__, static_folder='client/dist')
 # CORS(app) 
 
 # @app.route('/api/submit', methods=['POST'])
 # def submit_data():
-#     data = request.get_json()
-#     print('Received data:', data)
+#     pubsub_msg_dict = request.get_json()
+#     print('Received data:', pubsub_msg_dict)
+#     publish_message(PROJECT_ID, SCAN_TOPIC_ID, pubsub_msg_dict)
 #     return jsonify({'message': 'Data received successfully!'})
 
 
@@ -42,4 +43,3 @@ print(doc_ref.get().to_dict())
 # if __name__ == '__main__':
 #     # This is only for local testing. Deployment to Cloud Functions uses the `main` function.
 #     app.run(debug=True, host='localhost', port=8080)
-
